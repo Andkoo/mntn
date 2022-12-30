b8b44d0b500ade5ffb2af80800e66a7fc43a2e47
Andrej Mikulička <andrejmikulicka95@gmail.com>
2022-12-30 15:25:50 +0100

    feat: added social icons

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -1,3 +1,5 @@
+import { SocialIcon } from "react-social-icons";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -12,6 +14,23 @@ const DefaultLayout = ({ children }: TDefaultLayoutProps) => (
     <main className="min-h-screen">{children}</main>
 
     <Footer />
+
+    <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-end p-4 gap-x-2">
+      <SocialIcon
+        url="https://github.com/Andkoo/mntn"
+        target="_blank"
+        bgColor="hsl(0, 0%, 80%)"
+        fgColor="hsl(0, 0%, 40%)"
+        className="!h-8 !w-8 opacity-50 transition-opacity hover:opacity-100"
+      />
+      <SocialIcon
+        url="mailto:andrejmikulicka95@gmail.com"
+        target="_blank"
+        bgColor="hsl(0, 0%, 80%)"
+        fgColor="hsl(0, 0%, 40%)"
+        className="!h-8 !w-8 opacity-50 transition-opacity hover:opacity-100"
+      />
+    </div>
   </div>
 );
 
